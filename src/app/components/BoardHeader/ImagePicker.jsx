a07098737289e8d393dc82eacccbec18225b819e
Danie231 <danielezra10@gmail.com>
2019-03-25 16:53:12 +0200

    start working on board for image

---
+++ src/app/components/BoardHeader/ImagePicker.jsx
@@ -0,0 +1,65 @@
+import React, { Component } from "react";
+import PropTypes from "prop-types";
+import { connect } from "react-redux";
+import { withRouter } from "react-router-dom";
+import { Button, Wrapper, Menu, MenuItem } from "react-aria-menubutton";
+import classnames from "classnames";
+import FaCheck from "react-icons/lib/fa/check";
+import colorIcon from "../../../assets/images/color-icon.png";
+import "./ColorPicker.scss";
+import { withTranslation } from "react-i18next";
+
+class ImagePicker extends Component {
+  static propTypes = {
+    boardId: PropTypes.string.isRequired,
+    boardColor: PropTypes.string.isRequired,
+    dispatch: PropTypes.func.isRequired
+  };
+
+  handleSelection = color => {
+    const { dispatch, boardId, boardColor } = this.props;
+    // Dispatch update only if selected color is not the same as current board color.
+    if (color !== boardColor) {
+      dispatch({ type: "CHANGE_BOARD_COLOR", payload: { boardId, color } });
+    }
+  };
+
+  render() {
+    const { boardColor,t } = this.props;
+    const colors = ["blue", "green", "red", "pink"];
+    return (
+      <Wrapper
+        className="color-picker-wrapper"
+        onSelection={this.handleSelection}
+      >
+        <Button className="color-picker">
+          <img src={colorIcon} alt="colorwheel" className="modal-icon" />
+          <div className="board-header-right-text">
+            &nbsp;{t('Color')} &nbsp;&#9662;
+          </div>
+        </Button>
+        <Menu className="color-picker-menu">
+          {colors.map(color => (
+            <MenuItem
+              value={color}
+              className={classnames("color-picker-item", color)}
+              key={color}
+            >
+              {color === boardColor && <FaCheck />}
+            </MenuItem>
+          ))}
+        </Menu>
+      </Wrapper>
+    );
+  }
+}
+
+const mapStateToProps = (state, ownProps) => {
+  const { boardId } = ownProps.match.params;
+  return {
+    boardColor: state.boardsById[boardId].color,
+    boardId
+  };
+};
+
+export default withRouter(connect(mapStateToProps)(withTranslation()(ImagePicker)));
