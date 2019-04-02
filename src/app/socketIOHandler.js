6a52e9cc8e28a426ee69882e8f8dc7bbc1963e14
ShragaUser <dandankidon@gmail.com>
2019-04-02 22:35:54 +0300

    Update socketIOHandler.js
    removes comments

--- src/app/socketIOHandler.js
+++ src/app/socketIOHandler.js
@@ -1,15 +1,6 @@
 import socketIOClient from 'socket.io-client';
 import {SOCKETLOCATION} from "../constants";
 
-// const getSocket = () => {
-//     if(this.socket)
-//         return this.socket;
-//     this.socket = socketIOClient(SOCKETLOCATION);
-//     return this.socket;
-// }  
-
-// export default getSocket();
-
 const socket = socketIOClient(SOCKETLOCATION);
 
 export default socket;
