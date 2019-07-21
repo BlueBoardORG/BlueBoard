76a9cd6d83c028ad1680749f0d95dd065dcf31e7
liadperetz <liad.per@gmail.com>
2019-07-21 06:02:38 +0300

    commit

--- src/app/middleware/watchBoardMiddleware.js
+++ src/app/middleware/watchBoardMiddleware.js
@@ -7,10 +7,6 @@ const watchMiddleware = store => next => action => {
         user,
         currentBoardId: boardId,
       } = store.getState();
-    /*console.log("middleware: user: ");
-    console.log(user);
-    console.log("boardId: ");
-    console.log(boardId);*/
 
     let currentWatchMode = null;
 
@@ -18,17 +14,20 @@ const watchMiddleware = store => next => action => {
         currentWatchMode = store.getState().boardsById[boardId].users.find(boardUser=>boardUser.id === user._id).watch;
     }
 
+    console.log("action:");
+    console.log(action);
 
-    /*if (user && !action.dontPersist) {
-        if (!['PUT_BOARD_ID_IN_REDUX', 'UPDATE_FILTER','CHANGE_CARD_FILTER', 'LOAD_BOARD_USERS_DATA','SET_CURRENT_CARD','TOGGLE_SOCKET_CONNECTION','CHANGE_USER_WATCH'].includes(action.type)){
-            fetch("/api/history", {
-                method: "POST",
-                body: JSON.stringify({userId: user._id,boardId,action: action.type, payload: action.payload, socketId: socket.id}),
-                headers: { "Content-Type": "application/json" },
-                credentials: "include"
-              })
-        }   
-    }*/
+
+    if (user && !action.dontPersist) {
+        if (
+            [
+              "UPDATE_ASSIGNED_USER",
+              "ADD_USER",
+              "REMOVE_USER",
+              "CHANGE_USER_ROLE"
+            ].includes(action.type)
+          )
+    }
 };
 
 export default watchMiddleware;
