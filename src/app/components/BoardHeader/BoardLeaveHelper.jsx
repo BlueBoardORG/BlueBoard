847c97ce53f11feaffb1010eb1b2a53c1dfc7090
Inbal Malihi <inbalmlihi@gmail.com>
2019-08-13 15:52:58 +0300

    refactor(BoardLeaveHelper): removing changeUserRoleToAdmin function.

--- src/app/components/BoardHeader/BoardLeaveHelper.jsx
+++ src/app/components/BoardHeader/BoardLeaveHelper.jsx
@@ -6,52 +6,41 @@ import {
 
 
 export const adminLeaveHandler = (userId, users) => {
-    let newUsers = users;
-    if (!doesMoreAdmin(userId, users)) {
+    if (!doesAnotherAdminExist(userId, users)) {
         const firstReadWrite = findFirstUserInRole(READ_WRITE_ROLE, users)
         if (!firstReadWrite) {
             const firstRead = findFirstUserInRole(READ_ROLE, users);
             if (!firstRead)
-                return newUsers;
-            else
-                newUsers = changeUserRoleToAdmin(firstRead, users);
+                return users;
+            else 
+                users[firstRead].role = ADMIN_ROLE;
         }
         else
-            newUsers = changeUserRoleToAdmin(firstReadWrite, users);
+            users[firstReadWrite].role = ADMIN_ROLE;
     }
-    else{
-        console.log('MORE ADMIN');
-    }
-    return newUsers;
+    return users;
 }
 
 const findFirstUserInRole = (role, users) => {
     for (var i = 0; i < users.length; i++) {
         if (users[i].role === role) {
-            console.log('findFirstUserInRole', i);
             return i;
         }
     }
-    return null;
+    return;
 }
 
-const doesMoreAdmin = (userId, users) => {
-    for(var i=0; i<users.length; i++) {
-        if(users[i].id !== userId && users[i].role === ADMIN_ROLE)
-           return true; 
+const doesAnotherAdminExist = (userId, users) => {
+    for (var i = 0; i < users.length; i++) {
+        if (users[i].id !== userId && users[i].role === ADMIN_ROLE)
+            return true;
     }
     return false;
 }
 
-
-const changeUserRoleToAdmin = (userIndex, users) => {
-    users[userIndex].role = ADMIN_ROLE;
-    return users;
-}
-
-export const isCurrentUserAdmin = (userIdToRemove, users) => {
+export const isCurrentUserAdmin = (userId, users) => {
     for (var i = 0; i < users.length; i++) {
-        if (users[i].id === userIdToRemove)
+        if (users[i].id === userId)
             return users[i].role === ADMIN_ROLE;
     }
 }
