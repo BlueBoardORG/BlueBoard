f332bf8dc5c1f520843c33ef8c78369e72cf906a
ShragaUser <dandankidon@gmail.com>
2019-08-28 04:25:26 -0400

    Update authConfig.js

--- src/server/authConfig.js
+++ src/server/authConfig.js
@@ -1,3 +1,5 @@
+require("dotenv").config();
+
 const authConfig = {
     shragaURL: process.env.SHRAGA_URL || "http://localhost:3000",
     callbackURL: process.env.CALLBACK_URL || "http://localhost:1337/auth/shraga"
