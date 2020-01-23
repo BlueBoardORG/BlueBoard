378bf94854c94b79f26452741295d2dd44685960
adirsam14 <adir.samuel16@gmail.com>
2020-01-23 14:40:46 +0200

    feat(chatToBoard): add button that open iframe of hi chat in board

--- src/app/components/Board/Board.jsx
+++ src/app/components/Board/Board.jsx
@@ -2,6 +2,7 @@ import React, { Component } from "react";
 import PropTypes from "prop-types";
 import { connect } from "react-redux";
 import { Title } from "react-head";
+import Iframe from "react-iframe";
 import { DragDropContext, Droppable } from "react-beautiful-dnd";
 import classnames from "classnames";
 import { withTranslation } from "react-i18next";
@@ -15,6 +16,7 @@ import { CAN_EDIT_ROLES } from "../../../constants";
 import "./Board.scss";
 import BoardMenu from "../BoardHeader/BoardMenu";
 
+
 class Board extends Component {
   static propTypes = {
     lists: PropTypes.arrayOf(
@@ -27,14 +29,15 @@ class Board extends Component {
     dispatch: PropTypes.func.isRequired,
     boardUsers: PropTypes.array,
     boardLabels: PropTypes.array.isRequired,
-    cards: PropTypes.object.isRequired
+    cards: PropTypes.object.isRequired,
   };
 
   constructor(props) {
     super(props);
     this.state = {
       startX: null,
-      startScrollX: null
+      startScrollX: null,
+      iFrame:false
     };
   }
 
@@ -174,11 +177,17 @@ class Board extends Component {
       });
     });
   }
+  iFrameChange = () =>{
+    const {iFrame} = this.state;
+    this.setState({iFrame: !iFrame});
+  }
 
   render = () => {
 
     const { lists, boardTitle, boardId, boardColor, t, user, board, boardImageBackground, socketConnected } = this.props;
+    const {iFrame} = this.state;
     const imageUrl = `url(${boardImageBackground})`;
+    const hiUrl = `https://hi.prod.services.idf/channel/${encodeURIComponent(`${boardTitle}-${boardId}`)}`;
     const wrapperStyle = {
       backgroundImage: imageUrl,
       backgroundPosition: "center",
@@ -187,7 +196,6 @@ class Board extends Component {
     };
     const userData = board.users.find(u => u.id === user._id) || {};
     const isAbleToEdit = CAN_EDIT_ROLES.includes(userData.role) && socketConnected;
-
     const customStyles = {
       content: {
         top: '50%',
@@ -216,13 +224,18 @@ class Board extends Component {
             <h1 style={{ textAlign: "center", alignContent: "center", float: "none", margin: "auto" }}> {t("connection.wait")} </h1>
             <p style={{ textAlign: "center", alignContent: "center", float: "none", margin: "auto", paddingTop: "5vh" }}> {t("connection.wait.message")} </p>
           </Modal>
-          <BoardHeader isAbleToEdit={isAbleToEdit} />
+          
+          <BoardHeader isAbleToEdit={isAbleToEdit} iFrameAction={this.iFrameChange} />
           {/* eslint-disable jsx-a11y/no-static-element-interactions */}
           <div
             className="lists-wrapper"
             onMouseDown={this.handleMouseDown}
             onWheel={this.handleWheel}
           >
+           {iFrame ? <p>  <Iframe url= {hiUrl}
+           className="iframe"/>  </p> : null}
+                    
+
             {/* eslint-enable jsx-a11y/no-static-element-interactions */}
             <DragDropContext onDragEnd={this.handleDragEnd}>
               <Droppable
@@ -230,6 +243,7 @@ class Board extends Component {
                 type="COLUMN"
                 direction="horizontal"
               >
+                
                 {provided => (
                   <div className="lists" id="lists" ref={provided.innerRef}>
                     {lists.map((list, index) => (
--- src/app/components/BoardHeader/BoardHeader.jsx
+++ src/app/components/BoardHeader/BoardHeader.jsx
@@ -5,14 +5,17 @@ import ImagePicker from "./ImagePicker";
 import BoardDeleter from "./BoardDeleter";
 import BoardLeave from "./BoardLeave";
 import BoardArchiver from "./BoardArchiver";
+import hiIcon from "../../../assets/images/Hi.png";
 import Watch from "./Watch";
 
 import "./BoardHeader.scss";
 
-const BoardHeader = ({ isAbleToEdit }) => (
+const BoardHeader = ({ isAbleToEdit,iFrameAction }) => (
   <div className="board-header">
     <BoardTitle isAbleToEdit={isAbleToEdit} />
     <div className="board-header-right">
+    <div className="vertical-line" />
+    <img  style={{width: 40, height: 40}} onClick={iFrameAction} src={hiIcon} ></img>
       {isAbleToEdit && <div className="vertical-line" />}
       {isAbleToEdit && <Watch />}
       {isAbleToEdit && <div className="vertical-line" />}
