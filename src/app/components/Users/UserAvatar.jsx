8681a00c32df184e716b6723d72e30d25a7677bb
Almog Vagman Ciprut <almogvc@gmail.com>
2020-02-16 20:20:24 +0200

    refactor(UserAvatar): change user circle color

--- src/app/components/Users/UserAvatar.jsx
+++ src/app/components/Users/UserAvatar.jsx
@@ -6,6 +6,7 @@ import { Button, Wrapper, Menu, MenuItem } from "react-aria-menubutton";
 import { loadBoardUsersData } from "../../actions/boardActions";
 import { ADMIN_ROLE } from "../../../constants";
 import "./UsersList.scss";
+import { getColorFromString } from '../../helpers/colorFromString';
 
 const COLORS = ["red", "orange", "green", "blue", "purple", "black"];
 
@@ -15,7 +16,7 @@ class UserAvatar extends Component {
     if (action === t("UserAvatar.menu.kick")) {
       return this.deleteUser();
     }
-    return;
+
   };
 
   deleteUser = () => {
@@ -36,28 +37,27 @@ class UserAvatar extends Component {
   };
 
   hashCode = (str) => { // java String#hashCode
-    var hash = 0;
-    for (var i = 0; i < str.length; i++) {
-       hash = str.charCodeAt(i) + ((hash << 5) - hash);
+    let hash = 0;
+    for (let i = 0; i < str.length; i++) {
+      hash = str.charCodeAt(i) + ((hash << 5) - hash);
     }
     return hash;
-  } 
+  }
 
   intToRGB = (i) => {
-      var c = (i & 0x00FFFFFF)
-          .toString(16)
-          .toUpperCase();
+    const c = (i & 0x00FFFFFF)
+      .toString(16)
+      .toUpperCase();
 
-      return "00000".substring(0, 6 - c.length) + c;
+    return "00000".substring(0, 6 - c.length) + c;
   }
 
   render() {
     const { user, t, isCurrentUserAdmin, currentUser, boardUsers } = this.props;
 
     const words = user.name.split(" ");
-    const randomStyle = {
-      background: "#" + this.intToRGB(this.hashCode(user._id))
-    };
+    const color = getColorFromString(user.name || "")
+
     const deleteButton = (
       <div>
         <div className="board-leave-header">{t("UserAvatar.menu.title")}</div>
@@ -69,7 +69,7 @@ class UserAvatar extends Component {
       boardUsers.find(currUser => currUser.id === user._id) || {}
     ).role;
     // Adding the user's role to the tooltip
-    avatarToolTip += " (" + t("UserAvatar.role." + userRole) + ")";
+    avatarToolTip += ` (${t(`UserAvatar.role.${userRole}`)})`;
 
     return (
       <div>
@@ -78,7 +78,7 @@ class UserAvatar extends Component {
           onSelection={this.handleSelection}
         >
           <Button>
-            <span className="dot" style={randomStyle} data-tip={avatarToolTip}>
+            <span className="dot" style={{ backgroundColor: color }} data-tip={avatarToolTip}>
               {words[0][0]}
               {words.length > 1 && words[words.length - 1][0]}
             </span>
